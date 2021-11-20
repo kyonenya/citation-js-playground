2f5d5a2b24b12aea93bca9f57035580801a05376
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-11-21 02:14:55 +0900

    feat: ris形式をコードでparse

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const Cite = require('citation-js');
+
+const example = new Cite(`
+TY  - JOUR
+T1  - デリダの正義論：カント倫理学との対質
+A1  - 川谷茂樹
+Y1  - 2009
+PB  - 北海学園大学学術研究会
+ER  - 
+`);
+
+console.dir(example.data[0]);
+
+//{
+//  type: 'article-journal',
+//  title: 'デリダの正義論：カント倫理学との対質',
+//  author: [ { literal: '川谷茂樹' } ],
+//  issued: { 'date-parts': [ [ 2009 ] ] },
+//  publisher: '北海学園大学学術研究会',
+//  _graph: [/* */],
+//  id: 'temp_id_2960918811720883'
+//}
