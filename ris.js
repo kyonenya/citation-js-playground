095665b0216d8a494db493f8094b527b874b596c
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-11-21 21:23:33 +0900

    refactor: オブジェクトではなくタプルに

--- ris.js
+++ ris.js
@@ -2,46 +2,27 @@ const { read } = require('@customcommander/ris');
 
 const SEPARATOR = ', ';
 
-const result = read(`
-TY  - JOUR
-T1  - ウィトゲンシュタインは誰のための哲学者か
-A1  - G・E・M・アンスコム
-JO  - 現代思想
-IS  - 50(2)
-SP  - 89
-EP  - 97
-SN  - 0289-825X
-Y1  - 2021
-PB  - 青土社
-TA  - 吉田廉・京念屋隆史
-ER  -
-`)[0];
-
 function formatJpArticle(ris) {
-  const array = [
-    { value: ris.A1[0].last_name },
-    { value: ris.T1[0], modifier: (str) => `「${str}」` },
-    { value: `${ris.TA[0].last_name}訳${SEPARATOR}` },
-    { value: ris.JO[0], modifier: (str) => `『${str}』` },
-    { value: `${ris.IS[0]}: pp. ${ris.SP[0]}-${ris.EP[0]}` },
-    { value: ris.PB[0] },
-    { value: ris.Y1[0] },
+  const fragments = [
+    ris.A1[0].last_name,
+    [ris.T1[0], (str) => `「${str}」`],
+    `${ris.TA[0].last_name}訳${SEPARATOR}`,
+    [ris.JO[0], (str) => `『${str}』`],
+    `${ris.IS[0]}: pp. ${ris.SP[0]}-${ris.EP[0]}`,
+    ris.PB[0],
+    ris.Y1[0],
   ];
-  
-  const result = array.reduce((acc, val, i) => {
-    if (!array[i].modifier && (array[i + 1] && !array[i + 1].modifier)) {
-      return [...acc, val, { value: SEPARATOR }];
+
+  const separated = fragments.reduce((acc, val, i) => {
+    if (!Array.isArray(val) && (fragments[i + 1] && !Array.isArray(fragments[i + 1]))) {
+      return [...acc, val, SEPARATOR ];
     }
     return [...acc, val];
   }, []);
 
-  return result.map(v => v.modifier ? v.modifier(v.value) : v.value);
+  return separated.map(v => Array.isArray(v) ? v[1](v[0]) : v);
 }
 
-//console.log(formatJpArticle(result).join(''));
-//G・E・M・アンスコム「ウィトゲンシュタインは誰のための哲学者か」吉田廉・京念屋隆史訳, 『現代思想』50(2): pp. 89-97, 青土社, 2021
-
-//console.log(result[0]);
 //{
 //  TY: [ 'JOUR' ],
 //  T1: [ 'ウィトゲンシュタインは誰のための哲学者か' ],
--- test/ris.test.js
+++ test/ris.test.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 const { read } = require('@customcommander/ris');
 const { formatJpArticle } = require('../ris');
 
-describe('RIS', () => {
-  it('formatJpArticle', () => {
+describe('formatJpArticle', () => {
+  it('翻訳論文', () => {
     const translation = read(`
 TY  - JOUR
 T1  - ウィトゲンシュタインは誰のための哲学者か
@@ -22,4 +22,24 @@ ER  -
       'G・E・M・アンスコム「ウィトゲンシュタインは誰のための哲学者か」吉田廉・京念屋隆史訳, 『現代思想』50(2): pp. 89-97, 青土社, 2021'
     );
   });
+  
+//  it('論文', () => {
+//    const translation = read(`
+//TY  - JOUR
+//T1  - 考えるとき話しているのか
+//T2 - フッサール「独白」概念をめぐって
+//A1  - 京念屋隆史
+//JO  - 現象学年報
+//IS  - 37
+//SP  - 89
+//EP  - 97
+//SN  - 0289-825X
+//Y1  - 2021
+//PB  - 日本現象学会
+//`)[0];
+//    assert.strictEqual(formatJpArticle(translation).join(''),
+//      'G・E・M・アンスコム「ウィトゲンシュタインは誰のための哲学者か」吉田廉・京念屋隆史訳, 『現代思想』50(2): pp. 89-97, 青土社, 2021'
+//    );
+//  });
+
 });
