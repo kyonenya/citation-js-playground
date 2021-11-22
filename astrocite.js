d6fac0231367880538d90b0b0759fc9fc221802b
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-11-22 12:06:58 +0900

    fix: astrociteの著者名はカンマ区切り（とはいえ仕様にあるイニシャルがパースされないのでやっぱり不採用）

--- astrocite.js
+++ astrocite.js
@@ -29,18 +29,48 @@ year = {2007}
 const cslJson2 = parseRis(`
 TY  - JOUR
 T1  - デリダの正義論：カント倫理学との対質
-A1  - 川谷 茂樹
+A1  - 茂樹, 川谷
 Y1  - 2009
 PB  - 北海学園大学学術研究会
 ER  - 
 `);
-
-console.log(cslJson2[0]);
+//console.log(cslJson2[0]);
 //{
 //  id: 'h1a9dsofpa',
 //  type: 'article-journal',
 //  title: 'デリダの正義論：カント倫理学との対質',
 //  issued: { 'date-parts': [ [ '2009' ] ] },
 //  publisher: '北海学園大学学術研究会',
-//  author: [ { family: '' } ]
+//  author: [ { family: '川谷', given: '茂樹' } ]
+//}
+
+const cslJson3 = parseRis(`
+TY  - JOUR
+T1  - ウィトゲンシュタインは誰のための哲学者か
+A1  - Phillips, A. J.
+JO  - 現代思想
+IS  - 50(2)
+SP  - 89
+EP  - 97
+SN  - 0289-825X
+Y1  - 2021
+PB  - 青土社
+TA  - 廉, 吉田
+TA  - 隆史, 京念屋
+ER  -
+`);
+console.log(cslJson3[0]);
+//{
+//  id: 'h12bh5jsc4',
+//  type: 'article-journal',
+//  title: 'ウィトゲンシュタインは誰のための哲学者か',
+//  'container-title': '現代思想',
+//  issue: '50(2)',
+//  'page-first': '89',
+//  ISSN: '0289-825X',
+//  issued: { 'date-parts': [ [Array] ] },
+//  publisher: '青土社',
+//  author: [ { family: 'アンスコム', given: 'G・E・M・' } ],
+//  translator: [ { family: '廉', given: '吉田' }, { family: '隆史', given: '京念屋' } ],
+//  page: '89-97'
 //}
